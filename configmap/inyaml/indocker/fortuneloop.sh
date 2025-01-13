6e4a9eaf38c5008105509be82a82a9ceb3444932
gyungmean <yogurt2819@gmail.com>
2025-01-13 14:26:51 +0900

    add pv & configmap

---
+++ configmap/inyaml/indocker/fortuneloop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 매개변수 받지 않고 환경변수에서 바로 참조
+trap "exit" SIGINT
+echo "Configured to generate neew fortune every " $INTERVAL " seconds"
+mkdir /var/htdocs
+while :
+do
+    echo $(date) Writing fortune to /var/htdocs/index.html
+    /usr/games/fortune  > /var/htdocs/index.html
+    sleep $INTERVAL
+done
